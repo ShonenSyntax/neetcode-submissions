b7a546eb71b592885b54e923af8b06250f6056f1
Pratyaksh <reach.pratyaksh@gmail.com>
2026-09-22 19:23:35 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,38 @@
+//Practice_Session
+
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string encode(vector<string>& strs){
+        string encoded_string;
+
+        if(strs.empty())
+            return "\037";
+
+        for(int i=0;i<strs.size();i++){
+            if(i>0)
+                encoded_string += '\036'+strs[i];
+            else
+                encoded_string += strs[i];
+        }
+        return encoded_string;
+    }
+
+    vector<string> decode(string encoded_string){
+        vector<string> decoded_string;
+        
+        if(encoded_string == "\037")
+            return {};
+
+        int j=0;
+        for(int i=j;i<encoded_string.size();i++){
+            if(encoded_string[i]=='\036'){
+                decoded_string.push_back(encoded_string.substr(j,i-j));
+                j= i+1;
+            }
+        }
+        decoded_string.push_back(encoded_string.substr(j));
+        return decoded_string;
+    }
+};

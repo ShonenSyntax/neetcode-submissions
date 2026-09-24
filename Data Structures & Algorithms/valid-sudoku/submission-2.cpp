687f1c90ec0446405ee12ca22c89a3c83e026d39
Pratyaksh <reach.pratyaksh@gmail.com>
2026-09-25 00:44:16 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.cpp
@@ -0,0 +1,75 @@
+#include <set>
+using namespace std;
+
+bool check_row(vector<vector<char>>& board){
+
+    for(int i=0;i<board.size();i++){
+
+        set<char> my_row;
+
+        for(int j=0;j<board[0].size();j++){
+            if(board[i][j]=='.')
+                continue;
+            else if(my_row.find(board[i][j])!=my_row.end())
+                return false;
+            else
+                my_row.insert(board[i][j]);
+        }
+    }
+    return true;
+}
+
+bool check_column(vector<vector<char>>& board){
+
+    for(int j=0;j<board[0].size();j++){
+
+        set<char> my_column;
+
+        for(int i=0;i<board.size();i++){
+            if(board[i][j]=='.')
+                continue;
+            else if(my_column.find(board[i][j])!=my_column.end())
+                return false;
+            else
+                my_column.insert(board[i][j]);
+        }
+    }
+    return true;
+}
+
+bool check_box(vector<vector<char>>& board){ 
+    
+    for(int start_row=0;start_row<9;start_row+=3){
+        for(int start_column=0;start_column<9;start_column+=3){
+
+            set<char> my_box;
+
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+
+                    char current = board[start_row+i][start_column+j];
+
+                    if(current=='.')
+                        continue;
+
+                    else if(my_box.find(current)!=my_box.end())
+                        return false;
+
+                    else
+                        my_box.insert(current);
+                }
+            }
+        }
+    }
+    return true;
+}
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board){ //each cell is a char
+        if((check_row(board)) && (check_column(board)) && check_box(board))
+            return true;
+        else
+            return false;
+    }
+};
